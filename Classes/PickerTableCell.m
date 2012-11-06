e36ecaef02682cae98d704b4955b288d1b682622
Michael Schmidt <mschmidt.github@gmail.com>
2012-11-06 21:49:06 +0100

    variable font-size for picker cells (depends on short labels)

--- Classes/PickerTableCell.m
+++ Classes/PickerTableCell.m
@@ -112,12 +112,11 @@ static CGFloat const PickerViewCellHeight =  44.0;
     UILabel* label = (UILabel*)view;
 
     if (!label)
-    {
         label = [[UILabel alloc] init];
-        label.font = [UIFont boldSystemFontOfSize:18];
-        label.frame = CGRectMake (0.0f, 0.0f, PickerViewCellWidth-20, PickerViewCellHeight);
-        label.backgroundColor = [UIColor clearColor];
-    }
+
+    label.font = [UIFont boldSystemFontOfSize:(pickerShortLabels ? 18 : 20)];
+    label.frame = CGRectMake (0.0f, 0.0f, PickerViewCellWidth-20, PickerViewCellHeight);
+    label.backgroundColor = [UIColor clearColor];
     
     label.text = [self pickerView:pickerView titleForRow:row forComponent:component];
 
