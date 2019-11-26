29dc46dd75e07c912798dfa06c2ee42d78763d63
Ingmar Stein <IngmarStein@users.noreply.github.com>
2019-11-26 23:30:34 +0100

    fastlane: use default folder for metadata
    fastlane: remove unused Deliverfile
    Xcode project: s/UIKITFORMAC/MACCATALYST/

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -16,6 +16,8 @@ class Fastfile: LaneFile {
     // Update this, if you use features of a newer version
     var fastlaneVersion = "2.69.3"
 
+    let macAppIdentifier = "maccatalyst." + appIdentifier
+    let catalystDestination = "platform=macOS,arch=x86_64,variant=Mac Catalyst"
     let project = "Kraftstoff.xcodeproj"
     let scheme = "Kraftstoff"
     let screenshotScheme = "Fastlane UI Tests"
@@ -51,23 +53,31 @@ class Fastfile: LaneFile {
 
     func testMacOSLane() {
         desc("Runs all the tests")
-        runTests(project: project, scheme: scheme, sdk: "macosx", destination: "platform=macOS,arch=x86_64,variant=Mac Catalyst")
+        runTests(project: project, scheme: scheme, destination: catalystDestination)
     }
 
     private func buildIOS() {
         runTests(project: project, scheme: scheme)
         incrementBuildNumber()
         // syncCodeSigning(gitUrl: "gitUrl", appIdentifier: [appIdentifier], username: appleID)
-        captureScreenshots(project: project, languages: languages, scheme: screenshotScheme)
-        buildApp(project: project, scheme: scheme, configuration: "Release")
+        captureScreenshots(project: project,
+                           devices: devices,
+                           languages: languages,
+                           outputDirectory: "./fastlane/screenshots",
+                           scheme: screenshotScheme)
+        buildIosApp(project: project, scheme: scheme, configuration: "Release")
     }
 
     private func buildMacOS() {
-        //runTests(project: project, scheme: scheme, sdk: "macosx", destination: "platform=macOS,arch=x86_64,variant=Mac Catalyst")
+        runTests(project: project, scheme: scheme, destination: catalystDestination)
         //incrementBuildNumber()
         // syncCodeSigning(gitUrl: "gitUrl", appIdentifier: [appIdentifier], username: appleID)
-        //captureScreenshots(project: project, languages: languages, scheme: "Fastlane UI Tests", sdk: "macosx")
-        buildApp(project: project, scheme: scheme, configuration: "Release", sdk: "macosx", destination: "platform=macOS,arch=x86_64,variant=Mac Catalyst")
+        //captureScreenshots(project: project,
+        //                   devices: devices,
+        //                   languages: languages,
+        //                   outputDirectory: "./fastlane/screenshots-catalyst",
+        //                   scheme: screenshotScheme)
+        buildIosApp(project: project, scheme: scheme, configuration: "Release", destination: catalystDestination)
     }
 
     func betaLane() {
@@ -89,7 +99,7 @@ class Fastfile: LaneFile {
 
         buildIOS()
         uploadToAppStore(username: appleID, app: appIdentifier)
-        frameScreenshots()
+        frameScreenshots(path: "./fastlane/screenshots")
 
        //addGitTag(buildNumber: getVersionNumber())
     }
@@ -98,8 +108,11 @@ class Fastfile: LaneFile {
         desc("Deploy a new macOS version to the App Store")
 
         buildMacOS()
-        uploadToAppStore(username: appleID, platform: "macosx", app: "maccatalyst." + appIdentifier)
-        frameScreenshots()
+        uploadToAppStore(username: appleID,
+                         appIdentifier: macAppIdentifier,
+                         platform: "osx",
+                         screenshotsPath: "./fastlane/screenshots-catalyst",
+                         app: macAppIdentifier)
 
         //addGitTag(buildNumber: getVersionNumber())
     }
